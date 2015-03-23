b2956084e1ad2f0aaa45de7614dc88cbebe97dce
Marc Siso <marc.siso@hotmail.com>
2015-03-23 10:12:26 +0100

    seventh

--- src/cat/copenic/ex4/MainActivity.java
+++ src/cat/copenic/ex4/MainActivity.java
@@ -4,10 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
 public class MainActivity extends ActionBarActivity {
-/*Comment fsagfag	g*/ 
+/*Conflict branch 1*/ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
